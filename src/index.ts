d6889f10b2ef43d7ad1d442a5fda98ade2efe2a3
Bas Bieling <bas@scrumble.nl>
2024-02-24 13:44:23 +0100

    Minor style improvement

--- src/index.ts
+++ src/index.ts
@@ -51,7 +51,7 @@ function doIndexResponse(response: Http2ServerResponse, adblock: boolean) {
 
     if (adblock) {
         response.end(`
-            <h1 style="color: red;">adblock detected</h1>
+            <h1 style="color: red; text-transform: uppercase;">adblock detected</h1>
             <p>Please disable adblock so we can show you kittens</p>
         `);
     } else {
